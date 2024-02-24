f614fe8ad0feef29115d2aa840777391f1e18ff1
rmcpi <79471311+onaysphere@users.noreply.github.com>
2024-02-24 14:13:38 -0500

    ELEVPID AGAIN

--- 2024-Bot-Import/src/main/java/frc/robot/subsystems/Elevator.java
+++ 2024-Bot-Import/src/main/java/frc/robot/subsystems/Elevator.java
@@ -3,15 +3,68 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+import frc.robot.Constants;
+import frc.robot.Constants.MechConstants;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Elevator extends SubsystemBase {
+public class Elevator extends ProfiledPIDSubsystem {
+  private CANSparkMax m_ElevAngle;
+
+  private RelativeEncoder m_EncAngEncoder;
+
+  private ArmFeedforward m_feedforward;
+
   /** Creates a new Elevator. */
-  public Elevator() {}
+  public Elevator() {
+    super(
+      new ProfiledPIDController(
+        MechConstants.kPElevAngle,
+        MechConstants.kIElevAngle,
+        MechConstants.kIElevAngle,
+          new TrapezoidProfile.Constraints
+            (MechConstants.kElevAngleMaxVelocity, 
+            MechConstants.kElevAngleMaxAcceleration)
+        ), 
+        0);
+    m_ElevAngle = new CANSparkMax(8, MotorType.kBrushless);
+    m_feedforward = new ArmFeedforward(
+      getMeasurement(),
+      getMeasurement(),
+      getMeasurement(),
+      getMeasurement());
+
+    //Absolute
+    m_EncAngEncoder = m_ElevAngle.getEncoder();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  @Override
+  protected void useOutput(double output, State setpoint) {
+    // TODO Auto-generated method stub
+    // Calculate the feedforward from the sepoint
+    double feedforward = m_feedforward.calculate(setpoint.position, setpoint.velocity);
+    // Add the feedforward to the PID output to get the motor output
+    m_ElevAngle.setVoltage(output + feedforward);
+  }
+
+  @Override
+  protected double getMeasurement() {
+    // TODO Auto-generated method stub
+    return m_EncAngEncoder.getPosition() * MechConstants.kElevAngleConversionFactor;
   }
 }
